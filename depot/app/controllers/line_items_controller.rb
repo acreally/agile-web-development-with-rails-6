afc44131fa401bea483ac56921d2a64e271a87cf
Andrew Creally <creallya@gmail.com>
2020-10-17 20:01:27 -0400

    Dynamicalled update card in side panel when removing an item.

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create]
+  before_action :set_cart, only: [:create, :destroy]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -61,6 +61,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
     respond_to do |format|
       format.html { redirect_to cart_url(@line_item.cart.id) }
+      format.js
       format.json { head :no_content }
     end
   end
