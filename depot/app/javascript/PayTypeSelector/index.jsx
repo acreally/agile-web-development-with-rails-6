eae834b6a1006f6a68611153ddab58955b7d4189
Andrew Creally <creallya@gmail.com>
2020-11-11 17:21:32 -0500

    Starting to add custom rendering for different payment types.

--- depot/app/javascript/PayTypeSelector/index.jsx
+++ depot/app/javascript/PayTypeSelector/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
+import NoPayType from './NoPayType'
+import CreditCardPayType from './CreditCardPayType'
+import CheckPayType from './CheckPayType'
+import PurchaseOrderPayType from './PurchaseOrderPayType'
+// continue on pg 223
+
 class PayTypeSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onPayTypeSelected= this.onPayTypeSelected.bind(this)
+    this.state = { selectedPayType: null };
+  }
+
+  onPayTypeSelected(event) {
+    this.setState({ selectedPayType: event.target.value });
+  }
+
   render() {
     return (
       <div className="field">
         <label htmlFor="order_pay_type">Pay type</label>
-        <select id="order_pay_type" name="order[payment_type_id]">
+        <select id="pay_type" onChange={this.onPayTypeSelected} name="order[payment_type_id]">
           <option value="">Select a payment method</option> 
           <option value="0">Check</option>
           <option value="1">Credit card</option>
