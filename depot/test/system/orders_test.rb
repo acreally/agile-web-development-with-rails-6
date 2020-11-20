aa0093634073ec234eb5ce961e57e25ea7b9b11d
Andrew Creally <creallya@gmail.com>
2020-11-20 09:43:05 -0500

    Getting closer to getting creating an order system test working, but only when checking out from the store page, not when creating a new order from the orders page.

--- depot/test/system/orders_test.rb
+++ depot/test/system/orders_test.rb
@@ -10,15 +10,28 @@ class OrdersTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Orders"
   end
 
-  test "creating a Order" do
-    visit orders_url
-    click_on "New Order"
+  test "creating a Order using check as a payment method" do
+    # first, add an item to a cart because
+    # we cannot create an order with an empty cart
+    visit store_index_url
+    click_on "Add to Cart", match: :first
+    click_on "Checkout"
 
     fill_in "Address", with: @order.address
     fill_in "Email", with: @order.email
     fill_in "Name", with: @order.name
-    fill_in "Pay type", with: @order.payment_type
-    click_on "Create Order"
+
+    assert_selector "#order_pay_type"
+    assert_selector "option[value=\"1\"]"
+    assert_no_selector "#order_routing_number"
+
+    select "Check", from: "Pay type"
+
+    assert_selector "#order_routing_number"
+    fill_in "Routing #", with: "123"
+    fill_in "Account #", with: "12345678"
+
+    click_on "Place Order"
 
     assert_text "Order was successfully created"
     click_on "Back"
