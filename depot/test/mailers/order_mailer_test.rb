fc6a47c5cf860ccc9dc01acb3ac2ea149ee57e9f
Andrew Creally <creallya@gmail.com>
2020-11-30 09:30:52 -0500

    Sending order shipped email using HTML template.

--- depot/test/mailers/order_mailer_test.rb
+++ depot/test/mailers/order_mailer_test.rb
@@ -6,7 +6,7 @@ class OrderMailerTest < ActionMailer::TestCase
   end
 
   test "received" do
-    expected_body = /Dear #{@order.name}[[:space:]]+Thank you for your recent order from The Pragmatic Store.[[:space:]]+You ordered the following items:[[:space:]]+1 . MyString2[[:space:]]+We'll send you a separate email when your order ships./
+    expected_body = /Dear #{@order.name}\s+Thank you for your recent order from The Pragmatic Store.\s+You ordered the following items:\s+1 . MyString2\s+We'll send you a separate email when your order ships./
     mail = OrderMailer.received(@order)
     assert_equal "Pragmatic Store Order Confirmation", mail.subject
     assert_equal [@order.email], mail.to
@@ -15,7 +15,7 @@ class OrderMailerTest < ActionMailer::TestCase
   end
 
   test "shipped" do
-    expected_body = /Dear #{@order.name}[[:space:]]+Your order has been shipped.[[:space:]]+It will be delivered to:[[:space:]]+#{@order.address}/
+    expected_body = /<h3>Pragmatic Order Shipped<\/h3>\s+<p>\s+This is just to let you know that we've shipped your recent order:\s+<\/p>\s+<table>\s+<tr><th colspan="2">Qty<\/th><th>Description<\/th><\/tr>\s+<td[^>]*>1<\/td>\s+<td>MyString2<\/td>\s+<td[^>]*>\$9.99<\/td>\s+<\/table>/
     mail = OrderMailer.shipped(@order)
     assert_equal "Pramatic Store Order Shipped", mail.subject
     assert_equal [@order.email], mail.to
