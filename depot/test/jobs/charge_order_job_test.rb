2e5cececf95655b604d7902dc96d220e171b1307
Andrew Creally <creallya@gmail.com>
2020-12-02 09:12:22 -0500

    Add test for processing credit card and purchase order payments

--- depot/test/jobs/charge_order_job_test.rb
+++ depot/test/jobs/charge_order_job_test.rb
@@ -20,7 +20,45 @@ class ChargeOrderJobTest < ActiveJob::TestCase
       account: "12345678",
     }
 
-    Pago.expects(:make_payment).with(order_id: order.id, payment_method: :check, payment_details: expected_payment_details).returns(@payment_result)
+    assert_perform(expected_payment_details, :check, order, pay_type_params)
+  end
+
+  test "payment with a credit card sends details to payment processor and emails customer" do
+    order = orders(:two)
+    pay_type_params = {
+      credit_card_number: "4123123412341234",
+      expiration_date: "01/25",
+    }
+
+    expected_payment_details = {
+      cc_num: "4123123412341234",
+      expiration_month: "01",
+      expiration_year: "25",
+    }
+
+    assert_perform(expected_payment_details, :credit_card, order, pay_type_params)
+  end
+
+  test "payment with a purchase order sends details to payment processor and emails customer" do
+    order = orders(:three)
+    pay_type_params = {
+      po_number: "12345678",
+    }
+
+    expected_payment_details = {
+      po_num: "12345678",
+    }
+
+    assert_perform(expected_payment_details, :po, order, pay_type_params)
+  end
+
+  private
+
+  def assert_perform(expected_payment_details, expected_payment_method, order, pay_type_params)
+    Pago.expects(:make_payment).with(
+      order_id: order.id,
+      payment_method: expected_payment_method,
+      payment_details: expected_payment_details).returns(@payment_result)
     OrderMailer.expects(:received).with(order).returns(@mock_mailer)
     @mock_mailer.expects(:deliver_later)
 
